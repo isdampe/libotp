7541a07b2d51e73b78a3a9805e456d5aac273f49
Richard Denton <isdampe@gmail.com>
2018-10-31 11:06:08 +1100

    ENHANCE: Finalise otp_auto_crypt.

--- src/otp.c
+++ src/otp.c
@@ -6,7 +6,7 @@
 #include <sys/random.h>
 #endif
 
-int otp_xor(uint8_t *src, const size_t src_size, const uint8_t *key,
+int otp_crypt(uint8_t *src, const size_t src_size, const uint8_t *key,
 	const size_t key_size)
 {
 	if (key_size < src_size)
@@ -18,39 +18,41 @@ int otp_xor(uint8_t *src, const size_t src_size, const uint8_t *key,
 	return 0;
 }
 
-int otp_auto_xor(uint8_t *src, const size_t src_size, uint8_t *key_dest)
+struct otp_handle otp_auto_crypt(uint8_t *src, const size_t src_size)
 {
-	if (key_dest == NULL)
-		return EOTP_KEY_DEST_NULL;
+	struct otp_handle otp_result;
+	otp_result.key = malloc(src_size * sizeof(uint8_t));
+	if (otp_result.key == NULL) {
+		otp_result.result = EOTP_ALLOC;
+		return otp_result;
+	}
+
+	int bytes_generated = 0;
 
 #ifdef __linux__
 	int result = 0;
-	int bytes_generated = 0;
-	if (src_size > 256) {
-		int loop_iterations = src_size / 256;
-		size_t i=0;
+	int read_size = 256;
 
-		for (i=0; i<loop_iterations; ++i) {
-			result = getrandom(key_dest, 256, GRND_RANDOM);
-			if (result != 256)
-				return EOTP_GRAND;
-			bytes_generated += 256;
-			*key_dest += bytes_generated;
+	while (bytes_generated < src_size) {
+		read_size = ((src_size - bytes_generated) < 256 ? 
+			(src_size - bytes_generated) : 256);
+		result = getrandom(&otp_result.key[bytes_generated], read_size, GRND_RANDOM);
+		if (result < 0) {
+			otp_result.result = EOTP_GRAND;
+			return otp_result;
 		}
 
-		int final_step_n = (src_size % 256);
-		result = getrandom(key_dest, final_step_n, GRND_RANDOM);
-		bytes_generated += final_step_n;
-		*key_dest -= bytes_generated;
-	} else {
-		result = getrandom(key_dest, src_size, GRND_RANDOM);
-		bytes_generated += src_size;
+		bytes_generated += result;
 	}
-
-	if (result < 0)
-		return EOTP_GRAND;
-
 #endif
 
-	return otp_xor(src, src_size, key_dest, bytes_generated);
+	otp_result.key_size = bytes_generated;
+	otp_result.result = otp_crypt(src, src_size, otp_result.key, otp_result.key_size);
+
+	return otp_result;
+}
+
+void otp_close(struct otp_handle *handle)
+{
+	free(handle->key);
 }
--- src/otp.h
+++ src/otp.h
@@ -1,14 +1,21 @@
-#ifndef LIBONETIMEPAD_H
-#define LIBONETIMEPAD_H
+#ifndef LIBOTP_H
+#define LIBOTP_H
 #include <stddef.h>
 #include <stdint.h>
 
-#define EOTP_SIZE 0x1
-#define EOTP_KEY_DEST_NULL 0x2
-#define EOTP_GRAND 0x3
+#define EOTP_SIZE -1
+#define EOTP_GRAND -2
+#define EOTP_ALLOC -3
 
-int otp_xor(uint8_t *src, const size_t src_size, const uint8_t *key,
+struct otp_handle {
+	uint8_t *key;
+	size_t key_size;
+	int result;
+};
+
+int otp_crypt(uint8_t *src, const size_t src_size, const uint8_t *key,
 	const size_t key_size);
-int otp_auto_xor(uint8_t *src, const size_t src_size, uint8_t *key_dest);
+struct otp_handle otp_auto_crypt(uint8_t *src, const size_t src_size);
+void otp_close(struct otp_handle *handle);
 
 #endif
